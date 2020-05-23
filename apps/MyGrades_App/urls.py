b09de16ec0042d3c966725c8064fe5f25aaf98de
Jordan <www.jrdnbrj@hotmail.com>
2020-05-23 00:59:53 -0500

    Landing Page

--- apps/MyGrades_App/urls.py
+++ apps/MyGrades_App/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('post', post_assigment, name = 'post_assignment'),
-    path('post2', post_assigment2, name = 'post_assignment_2'),
+    path('post2', post_assigment_2, name = 'post_assignment_2'),
+    path('', landing_page, name = 'landing_page'),
 ]
--- apps/MyGrades_App/views.py
+++ apps/MyGrades_App/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render, redirect
 def post_assigment(request):
     return render(request, 'post/post_assignment.html', {})
 
-def post_assigment2(request):
+def post_assigment_2(request):
     return render(request, 'post/post_assignment_2.html', {})
+
+def landing_page(request):
+    return render(request, 'landing_page/landing_page.html', {})
