9e3cbbcafc97a293fe0fecc6bb47cfb2c50e80c8
Jordan <www.jrdnbrj@hotmail.com>
2020-09-07 02:53:51 -0500

    Designs and Logo

--- apps/MyGrades_App/urls.py
+++ apps/MyGrades_App/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
 
     path('work_place/', work_place, name='work_place'),
     path('wp_ajax', wp_ajax, name='wp_ajax'),
-    path('work_place_2/<int:pk>', work_place_2, name='work_place_2'),
+    path('work_place/<int:pk>', work_place_2, name='work_place_2'),
     path('work_place_3/<int:id>', work_place_3, name='work_place_3'),
     path('work_place_4/<int:id>', work_place_4, name='work_place_4'),
 
--- apps/MyGrades_App/views.py
+++ apps/MyGrades_App/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, Http404
 from django.core import serializers
@@ -176,14 +176,15 @@ def wp_ajax(request):
 
 @login_required
 def work_place_2(request, pk):
-    trabajo = Trabajo.objects.get(pk = pk)
-    if trabajo.trabajador: raise Http404()
+    # trabajo = Trabajo.objects.get(pk = pk)
+    trabajo = get_object_or_404(Trabajo, pk=pk)
+    if trabajo.estado != 'published': raise Http404('Que fue veeeee')
 
     return render(request, 'work_place/work_place_2.html', {'trabajo': trabajo})
 
 @login_required
 def work_place_3(request, id):
-    trabajo = Trabajo.objects.get(id = id)
+    trabajo = get_object_or_404(Trabajo, id=id)
     fecha_expiracion = trabajo.fecha_expiracion
     if trabajo.trabajador: raise Http404()
 
